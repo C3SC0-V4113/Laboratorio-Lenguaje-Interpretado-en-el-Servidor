528e68aec357e8ed5d4afea0c756711539a1dba8
Francisco Valle <vallecesco@gmail.com>
2021-01-30 18:38:33 -0600

    Ejercicio 2 y 3 completado

---
+++ Guia 2-Introducción a la Programación web con PHP/Ejercicio 2/uso_variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de PHP</title>
+    <style>
+        body{
+            font-family: "Arial Black",Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        //Declaracion de variables
+        $a=1;
+        $b=3.34;
+        $c="Hola Mundo";
+        echo $a,"<br>",$b,"<br>",$c;
+    ?>
+</body>
+</html>
