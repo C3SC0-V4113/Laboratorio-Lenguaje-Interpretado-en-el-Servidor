4e8e619d748fe8429194a4ff46563cc6564631fd
Francisco Valle <vallecesco@gmail.com>
2021-01-30 18:46:32 -0600

    Ejercicio 4 completado

---
+++ Guia 2-Introducción a la Programación web con PHP/Ejercicio 4/opera_comparacion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de PHP - Operadores de Comparacion</title>
+    <style>
+        body{
+            font-family: "Arial Black",Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $a=8;
+        $b=3;
+        $c=3;
+        echo $a==$b,"<br>";  
+        echo $a!=$b,"<br>";  
+        echo $a<$b,"<br>";  
+        echo $a>$b,"<br>";  
+        echo $a>=$b,"<br>";  
+        echo $a<=$b,"<br>";  
+    ?>
+</body>
+</html>
