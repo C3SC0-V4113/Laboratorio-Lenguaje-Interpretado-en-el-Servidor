bf1df4ce665a6895f67becd9fb2e99373db40b0b
Francisco Valle <vallecesco@gmail.com>
2021-01-30 21:08:27 -0600

    Empezando Ejercicio 7

---
+++ Guia 2-Introducción a la Programación web con PHP/Ejercicio 7/procesar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Información Recibida</title>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Nobile">
+    <link rel="stylesheet" href="css/tables.css">
+    <script src="js/modernizr.custom.lis.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
