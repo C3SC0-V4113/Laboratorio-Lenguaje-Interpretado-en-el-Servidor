e5c273ead62d65236f525ff4c15a424f82eedfd3
Francisco Valle <vallecesco@gmail.com>
2021-01-30 18:50:09 -0600

    Terminado Ejercicio 5 y empezado Ejercicio 6

---
+++ Guia 2-Introducción a la Programación web con PHP/Ejercicio 6/tiposdatos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversión de cadenas</title>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Cabin+Condensed:600">
+    <link rel="stylesheet" media="screen" href="css/conversion.css">
+</head>
+<body>
+    <?php
+        //Definiendo los valores a utilizar para los cálculos
+        define("SALTO","\n");
+        $cad="10 metros";
+        $str="3D";
+        $val=20.5;
+        $num=7;
+
+        //Construyendo el header
+        
+    ?>
+</body>
+</html>
