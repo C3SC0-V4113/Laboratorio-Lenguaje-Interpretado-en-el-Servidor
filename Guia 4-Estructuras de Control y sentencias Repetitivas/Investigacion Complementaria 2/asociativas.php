653fbdc1be4a4738b6589b7db7aca7e1b6ec574f
Francisco Valle <vallecesco@gmail.com>
2021-02-18 18:48:47 -0600

    Investigacion 2 Funcional

---
+++ Guia 4-Estructuras de Control y sentencias Repetitivas/Investigacion Complementaria 2/asociativas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matrices Asociativas</title>
+</head>
+<body>
+    <h1>Matrices Asociativas</h1>
+    <article>
+        <p>Los índices no tienen por qué ser correlativos, ni siquiera tienen por qué ser números.</p>
+    </article>
+    <section>
+        <?php
+        $materias=[
+            'Programación'=>'Lenguaje Interpretado en EL Servidor',
+            'Ciencias Básicas'=>'Ecuaciones Diferenciales',
+            'Diseño'=>'Dibujo y Arte Conceptual',
+            'Ciencias Economicas'=>'Economia 1',
+            'Aeronautica'=>'Matematicas 3',
+            'Escuela Industrial'=>'Analisis de Evaluación Economica'
+        ];
+
+        echo "<table>";
+        foreach ($materias as $key => $value) {
+            echo "\t<tr>";
+            echo "<th>$key</th><th>$value</th>";
+            echo "\t</tr>";
+        }
+        echo "</table>";
+        ?>
+    </section>
+    <footer>
+        <p>Copyright CESCO 2021</p>
+    </footer>
+</body>
+</html>
