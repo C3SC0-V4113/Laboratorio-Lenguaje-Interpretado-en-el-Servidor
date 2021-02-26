983155cf927fd8d8e01cfb2fd3cc30118569946e
Francisco Valle <vallecesco@gmail.com>
2021-02-25 22:01:41 -0600

    Pequeñas correcciones en ejercicio 2

--- Guia 5-Arreglos/Ejercicio 2/ejemplo2.php
+++ Guia 5-Arreglos/Ejercicio 2/ejemplo2.php
@@ -5,9 +5,9 @@
     <title>Uso de la función range</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">'
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>'
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>'
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 </body>
 <div class="container">
