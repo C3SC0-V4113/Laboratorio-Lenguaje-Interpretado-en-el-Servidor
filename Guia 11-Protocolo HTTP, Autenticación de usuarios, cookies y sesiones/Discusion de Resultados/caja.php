5ce6e5fd69fe64d1d86cd24516d79eca26f8f35a
Francisco Valle <vallecesco@gmail.com>
2021-04-30 10:13:42 -0600

    Copiando Ejercicio 2

---
+++ Guia 11-Protocolo HTTP, Autenticación de usuarios, cookies y sesiones/Discusion de Resultados/caja.php
@@ -0,0 +1,30 @@
+<?php
+ //Incluir librería con las funciones auxiliares
+ require_once("funciones.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP [hidden y urls]: caja.php</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body class="container">
+<section>
+<article>
+<?php
+ echo "<h1 align=\"center\">Contenido del carrito</h1>\n";
+ //Recuperar los objetos pertenecientes al carrito
+ $carrito = generarCarrito();
+ //Mostrar el contenido
+ mostrarCarrito($carrito);
+?>
+<p align="center">
+ Pulsa <a href="./tienda.php">aquí</a> para continuar.
+</p>
+</article>
+</section>
+</body>
+</html>
